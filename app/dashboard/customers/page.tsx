ada32ed98b5aa166d9b0b6ba46e1c98756ce8483
Márton Molnár <marton3molnar@gmail.com>
2023-10-30 09:58:05 +0100

    List customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,18 @@
-export default function Page(){
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomers, fetchFilteredCustomers, fetchInvoicesPages } from '@/app/lib/data';
+
+export default async function Page({ searchParams, }: { searchParams?: { query?: string; page?: string; }; }) {
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
     return (
-        <p>Customers page</p>
-    )
+        <Table customers={customers} />
+    );
 }
